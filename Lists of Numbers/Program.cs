4434a7f5c1582e5f48acd17acd4a2982c6541c26
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-10 09:39:02 -0400

    Not Done

--- Lists of Numbers/Program.cs
+++ Lists of Numbers/Program.cs
@@ -5,16 +5,49 @@
         static void Main(string[] args)
         {
             List<int> numbers = new List<int>(); // Creates an empty List
-            for (int i = 0; i < 10; i++)  
+            int better5 = 0;
+            int rand;
+            Random generator = new Random();
+            rand = generator.Next();
+            for (int i = 0; i < 20; i++)
             {
-                Console.WriteLine("Please enter a number: ");
-                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+                numbers.Add(rand);
+                if (numbers[i] >= 5)
+                {
+                    better5 += 1;
+                }
+                
             }
-            Console.Write(numbers[0]);
+            Console.WriteLine();
+            Console.Write(numbers[0] + " ");
+            Console.Write(numbers[1] + " ");
+            Console.Write(numbers[2] + " ");
+            Console.Write(numbers[3] + " ");
+            Console.Write(numbers[4] + " ");
+            Console.Write(numbers[5] + " ");
+            Console.Write(numbers[6] + " ");
+            Console.Write(numbers[7] + " ");
+            Console.Write(numbers[8] + " ");
+            Console.Write(numbers[9] + " ");
+            Console.Write(numbers[10] + " ");
+            Console.Write(numbers[11] + " ");
+            Console.Write(numbers[12] + " ");
+            Console.Write(numbers[13] + " ");
+            Console.Write(numbers[14] + " ");
+            Console.Write(numbers[15] + " ");
+            Console.Write(numbers[16] + " ");
+            Console.Write(numbers[17] + " ");
+            Console.Write(numbers[18] + " ");
+            Console.Write(numbers[19] + " ");
+
+
+
+            Console.WriteLine();
             Console.WriteLine("The largest number is " + numbers.Max()); 
             Console.WriteLine("The smallest number is " + numbers.Min()); 
             Console.WriteLine("The sum is " + numbers.Sum());
             Console.WriteLine("The average is " + numbers.Average());
+            Console.WriteLine("There were " + better5 + " numbers greater than 5");
 
             Console.ReadLine(); // Keeps the program from ending  
 
