78c5abad58733af57e179e689502d35fa3f193c5
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-15 09:11:15 -0400

    finished

--- Lists of Numbers/Program.cs
+++ Lists of Numbers/Program.cs
@@ -7,38 +7,47 @@
             List<int> numbers = new List<int>(); // Creates an empty List
             int better5 = 0;
             int rand;
+            int counter = -1;
+            bool done = false;
             Random generator = new Random();
-            rand = generator.Next();
-            for (int i = 0; i < 20; i++)
+            while (!done)
             {
-                numbers.Add(rand);
-                if (numbers[i] >= 5)
+                numbers.Add((generator.Next(41) * 2));
+                counter += 1;
+
+
+                if (numbers[counter] <= 50)
                 {
-                    better5 += 1;
+                    Console.WriteLine(0);
                 }
+                else
+                {
+                    Console.WriteLine(numbers[counter]);
+                }
+
+                    if (counter > 20) 
+                    done = true;
                 
             }
             Console.WriteLine();
-            Console.Write(numbers[0] + " ");
-            Console.Write(numbers[1] + " ");
-            Console.Write(numbers[2] + " ");
-            Console.Write(numbers[3] + " ");
-            Console.Write(numbers[4] + " ");
-            Console.Write(numbers[5] + " ");
-            Console.Write(numbers[6] + " ");
-            Console.Write(numbers[7] + " ");
-            Console.Write(numbers[8] + " ");
-            Console.Write(numbers[9] + " ");
-            Console.Write(numbers[10] + " ");
-            Console.Write(numbers[11] + " ");
-            Console.Write(numbers[12] + " ");
-            Console.Write(numbers[13] + " ");
-            Console.Write(numbers[14] + " ");
-            Console.Write(numbers[15] + " ");
-            Console.Write(numbers[16] + " ");
-            Console.Write(numbers[17] + " ");
-            Console.Write(numbers[18] + " ");
-            Console.Write(numbers[19] + " ");
+            Console.WriteLine();
+            counter = -1;
+            for (int i = 0; i < 20; i++)
+            {
+                counter += 1;
+                if (numbers[counter] == 10)
+                    Console.Write("TEN, ");
+                else if (numbers[counter] == 20)
+                    Console.Write("TWENTY, ");
+                else if (numbers[counter] == 30)
+                    Console.Write("THIRTY, ");
+                else if (numbers[counter] == 40)
+                    Console.Write("FOURTY, ");
+                else
+                    Console.Write(numbers[counter] + ", ");
+
+
+            }
 
 
 
@@ -47,7 +56,6 @@
             Console.WriteLine("The smallest number is " + numbers.Min()); 
             Console.WriteLine("The sum is " + numbers.Sum());
             Console.WriteLine("The average is " + numbers.Average());
-            Console.WriteLine("There were " + better5 + " numbers greater than 5");
 
             Console.ReadLine(); // Keeps the program from ending  
 
